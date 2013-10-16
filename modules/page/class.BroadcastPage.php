c354c9bf5595bc0cc511347a6bf47d5837306923
Anthony Ma <tuogex@yahoo.com>
2013-10-16 15:21:27 -0500

    UI for broadcast page.  SMS sending not yet implemented.

--- modules/page/class.BroadcastPage.php
+++ modules/page/class.BroadcastPage.php
@@ -8,9 +8,22 @@ class BroadcastPage extends Page {
 	public function writePageContent() {
 		$content = 
 '
+<script>
+window.onload = function () {
+	$("#messageHolder").keyup(function() {
+		$("#charCount").html(160 - $(this).val().length);
+	});
+};
+</script>
 <div class="jumbotron" style="font-size: medium;">
 <h1>Broadcast</h1>
-This page is under construction.
+<textarea class="form-control" rows="3" id="messageHolder" placeholder="Message (160 Character limit)"></textarea>
+<span style="float: right;" id="charCount">160</span>
+<button type="button" class="btn btn-primary btn-lg btn-block" style="margin-bottom: 20px;">Send</button>
+<div class="panel panel-info">
+<div class="panel-heading">Rodney API</div>
+Details soon
+</div>
 </div>
 ';
 		echo $content;
