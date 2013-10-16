b93b69872e97e681aa285c79fe8723136bfd222c
Anthony Ma <tuogex@yahoo.com>
2013-10-16 16:22:39 -0500

    Implement SMS broadcast. Permissions for confirming users and broadcasting SMS.

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
-define("MYSQL_HOST", "db430924922.db.1and1.com");
+define("MYSQL_HOST", "");
 define("MYSQL_PORT", 3306);
-define("MYSQL_DB", "db430924922");
-define("MYSQL_USER", "dbo430924922");
-define("MYSQL_PASSWORD", "1nn0c3nts_Twa1n");
-define("DB_USER_TABLE", "new_users");
+define("MYSQL_DB", "");
+define("MYSQL_USER", "");
+define("MYSQL_PASSWORD", "");
+define("DB_USER_TABLE", "");
 define("DB_USER_HASH_ALGO", "sha256");
 
 define("CHECKIN_MAX", 10);
+
+define("TROPO_MESSAGE_TOKEN", "");
 ?>
--- modules/page/class.BroadcastPage.php
+++ modules/page/class.BroadcastPage.php
@@ -17,13 +17,12 @@ window.onload = function () {
 </script>
 <div class="jumbotron" style="font-size: medium;">
 <h1>Broadcast</h1>
-<textarea class="form-control" rows="3" id="messageHolder" placeholder="Message (160 Character limit)"></textarea>
+This form will send a SMS message to all members with the receive texts option.
+<form method="POST">
+<textarea class="form-control" rows="3" id="messageHolder" name="message" placeholder="Message (160 Character limit)"></textarea>
 <span style="float: right;" id="charCount">160</span>
-<button type="button" class="btn btn-primary btn-lg btn-block" style="margin-bottom: 20px;">Send</button>
-<div class="panel panel-info">
-<div class="panel-heading">Rodney API</div>
-Details soon
-</div>
+<button type="submit" class="btn btn-primary btn-lg btn-block" style="margin-bottom: 20px;">Send</button>
+</form>
 </div>
 ';
 		echo $content;
@@ -31,8 +30,26 @@ Details soon
 
 	public function writePage() {
 		self::writePageStart();
+		if (array_key_exists("message", $_POST)) {
+			$url = 'http://api.tropo.com/1.0/sessions?action=create&token=' . TROPO_MESSAGE_TOKEN . '&numbers=' . $this->getFormattedNumbers() . '&msg=' . $_POST['message'];
+  			$xml = simplexml_load_file($url) or $this->alert("danger", "Error!", "Tropo API not responding.");
+  			if ((string)$xml->success === "true") {
+  				$this->alert("success", "Yay!", "Message broadcasted to all members.");
+  			}
+		}
 		self::writePageContent();
 		self::writePageEnd();
+	}
+
+	private function getFormattedNumbers() {
+		global $core;
+		$numbersStr = "";
+		$teamArr = $core->fetchAllUsers();
+		foreach ($teamArr as $member) {
+			$numbersStr .= ($member['text'] == 1 ? $member['phone'] . '|' : '');
+		}
+		$numbersStr = substr($numbersStr, 0, strlen($numbersStr) - 1);
+		return $numbersStr;
 	}
 }
 ?>
