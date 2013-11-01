db54e6ee2ee3647c5a089fece7c1dec64dd2a8b5
Eric Osburn <bjonesen@gmail.com>
2013-10-31 19:26:03 -0500

    Update DBConvert.php

--- assets/php/DBConvert.php
+++ assets/php/DBConvert.php
@@ -5,7 +5,7 @@
         $phone = str_replace("-", "", $oldMember->phonenumber);
         $phone = str_replace("(", "", $oldMember->phonenumber);
         $phone = str_replace(")", "", $oldMember->phonenumber);
-        $db->query("INSERT INTO  `new_users` (`rank` ,  `studentId` ,  `name` ,  `email` ,  `hours` ,  `phone` ,  `password` ,  `lastHourLog` ,  `text` ) VALUES ('" . floor($oldMember->rank / 10) . "'. '" . $oldMember->checkinid . "'.  '" . $oldMember->name . "'.  '" . $oldMember->email . "'.  '" . $oldMember->hours."'.'".$phone."'.'37a8eec1ce19687d132fe29051dca629d164e2c4958ba141d5'.'0'.'".$oldMember->phonetexting."'");
+        $db->query("INSERT INTO  `new_users` (`rank` ,  `studentId` ,  `name` ,  `email` ,  `hours` ,  `phone` ,  `password` ,  `lastHourLog` ,  `text` ) VALUES ('" . floor($oldMember->rank / 10) . "', '" . $oldMember->checkinid . "',  '" . $oldMember->name . "', '" . $oldMember->email . "', '" . $oldMember->hours."',".$phone."','37a8eec1ce19687d132fe29051dca629d164e2c4958ba141d5','0','".$oldMember->phonetexting."')");
         echo "INSERT INTO  `new_users` (`rank` ,  `studentId` ,  `name` ,  `email` ,  `hours` ,  `phone` ,  `password` ,  `lastHourLog` ,  `text` ) VALUES ('" . floor($oldMember->rank / 10) . "'. '" . $oldMember->checkinid . "'.  '" . $oldMember->name . "'.  '" . $oldMember->email . "'.  '" . $oldMember->hours."'.'".$phone."'.'37a8eec1ce19687d132fe29051dca629d164e2c4958ba141d5'.'0'.'".$oldMember->phonetexting."'\n";
     }
 ?>
