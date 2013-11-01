792be9661268246b1086268909d9d91078ce8cdb
Anthony Ma <tuogex@yahoo.com>
2013-11-01 13:33:46 -0500

    Bind param text in updateContactDetails

--- modules/class.Core.php
+++ modules/class.Core.php
@@ -81,6 +81,7 @@ class Core {
 		$stmt->bindParam(":phone", $phone);
 		$stmt->bindParam(":studentId", $studentId);
 		$stmt->bindParam(":id", $id);
+		$stmt->bindParam(":text", $text);
                 $stmt->execute();
 	}
 
