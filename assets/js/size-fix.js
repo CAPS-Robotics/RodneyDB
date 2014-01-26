f8b085370dcb657285dd2a09707506a31081752c
dmattt98 <dmattt98@gmail.com>
2014-01-26 00:06:24 -0600

    load background after page

--- assets/js/size-fix.js
+++ assets/js/size-fix.js
@@ -5,3 +5,11 @@ $( document ).ready(function() {
 $(window).resize(function (e){
 	$('#wrap').css('padding-top', ($('.navbar').height() - 40) + "px")
 });
+
+/* =========================== *\
+|  Nothing to do with size fix  |
+|  loads background after page  |
+\* =========================== */ 
+$(function () {
+	$('body').css('background', 'url(assets/img/wallpaper2.jpg)');
+});
