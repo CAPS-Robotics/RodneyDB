0f965515ac167866c679d4530e21e98db6439678
tgiv014 <tgiv014@gmail.com>
2014-01-30 19:41:27 -0600

    Semi-proper session fix

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ switch ($_GET['p']) {
 	case "login":
 		if ($_SESSION['loggedIn']) {
 			$_SESSION['loggedIn'] = false;
-			$_SESSION['email'] = 'THISISREALLYINSECURETONY';
+			unset($_SESSION['email']);
 			header("Location: ?p=home");
 		}
 		else {
