2d1bda4b8c83c362d8d64cf05d05e9e08486ad42
tgiv014 <tgiv014@gmail.com>
2014-01-30 18:33:59 -0600

    Only report top 10

--- modules/page/class.Json.php
+++ modules/page/class.Json.php
@@ -19,6 +19,7 @@ class Json extends Page {
 					$hours[] = $value['hours'];
 				}
 				array_multisort($hours,SORT_DESC,$users);
+				$users=array_slice($users,0,9);
 				var_dump($users);
 				$data = array(
 				    'success'=>'true',
