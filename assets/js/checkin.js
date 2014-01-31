b261d4b9899b7f6fdaa71af456bcfc27e7031a00
tgiv014 <tgiv014@gmail.com>
2014-01-30 22:35:45 -0600

    I have to admit it's getting better

--- assets/js/checkin.js
+++ assets/js/checkin.js
@@ -1,8 +1,10 @@
 $(window).load(function(){
+    $('#studentid').focus();
     var l;
     handleresponse = function (msg) {
         console.log(msg);
         l.stop();
+        $('#studentid').val('').focus();
         var level='danger';
         var title='Uh-Oh...';
         var description='There may be a problem with your account. Please contact an administrator if this keeps happening.';
@@ -23,18 +25,22 @@ $(window).load(function(){
         if(msg.code=="false"){
             description='The entered ID is either too short or too nonexistant.';
         }
-        $('#alertbox').fadeOut(200,function(){
-            $('#alertbox').html('<div class="alert alert-'+level+'" style="margin-top: -7px;"><strong>'+title+'</strong> '+description+'</div>');
-            $('#alertbox').fadeIn(200);
-        });
+        $('#alertbox').html('<div class="alert alert-'+level+'" style="margin-top: -7px;"><strong>'+title+'</strong> '+description+'</div>');
+        $('#alertbox').fadeIn(200);
     }
     $('#checkin').click(function () {
         l = Ladda.create(this);
         l.start();
+        $('#alertbox').fadeOut(100);
         var studentid=$('#studentid').val();
         $.ajax({
             type: "GET",
             url: '/repositories/RodneyDB/json/?p=json&r=checkin&d='+studentid
         }).done(handleresponse).fail(function(){console.log('Something has gone very wrong')});
     });
+    $("#studentid").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#checkin").click();
+    }
+});
 });
