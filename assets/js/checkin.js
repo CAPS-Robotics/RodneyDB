d59fd04e2d2992842c118802091ed345267357dd
tgiv014 <tgiv014@gmail.com>
2014-01-30 22:07:26 -0600

    Notifications!

--- assets/js/checkin.js
+++ assets/js/checkin.js
@@ -4,9 +4,23 @@ $(window).load(function(){
         console.log(msg);
         l.stop();
         var level='danger';
-        var title='OH NO!';
-        var description='YOU DUN GOOFED, SON';
-        $('#alertbox').html('<div id="alert" class="alert alert-'+level+'" style="margin-top: -7px;"><strong>'+title+'</strong> '+description+'</div>');
+        var title='Uh-Oh...';
+        var description='There may be a problem with your account. Please contact an administrator if this keeps happening.';
+        if(msg.code=="checkin"){
+            level='success';
+            title=msg.name+',';
+            description='you are now checked in. Don\'t forget to check out when the meeting is over!';
+        }
+        if(msg.code=="checkout"){
+            level='success';
+            title=msg.name+',';
+            description='you have checked out. You now have '+msg.hours+' hours.';
+        }
+        if(msg.code=="exceed"){
+            level='warning';
+            description='You\'ve been checked in too long! Hours from the last session will not be counted. Input again if you need to be checked in.';
+        }
+        $('#alertbox').html('<div class="alert alert-'+level+'" style="margin-top: -7px;"><strong>'+title+'</strong> '+description+'</div>');
     }
     $('#checkin').click(function () {
         l = Ladda.create(this);
