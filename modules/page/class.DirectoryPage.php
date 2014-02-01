9807bff777ed7a0bb41424ffc474703a333dd3f6
dmattt98 <dmattt98@gmail.com>
2014-02-01 11:06:50 -0600

    don't forget those script tags

--- modules/page/class.DirectoryPage.php
+++ modules/page/class.DirectoryPage.php
@@ -19,7 +19,9 @@ class DirectoryPage extends Page {
 ';
 		$script =
 '
+<script>
 $(\'span.editable\').on(\'click\', function() { var $this = $(this), var value = $this.text(); $this.html(\'<input type="text" class="form-control" value="\' + value + \'" autofocus autocomplete="off">\') });
+</script>
 ';
 		echo $content, $script;
 	}
