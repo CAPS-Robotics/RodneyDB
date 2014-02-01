213c8691e391ecd66f5a2cd65963b4cd7b0ca42a
dmattt98 <dmattt98@gmail.com>
2014-02-01 11:04:04 -0600

    add text box on click

--- modules/page/class.DirectoryPage.php
+++ modules/page/class.DirectoryPage.php
@@ -17,7 +17,12 @@ class DirectoryPage extends Page {
 </table>
 </div>
 ';
+		$script =
+'
+$(\'span.editable\').on(\'click\', function() { var $this = $(this), var value = $this.text(); $this.html(\'<input type="text" class="form-control" value="\' + value + \'" autofocus autocomplete="off">\') });
+'
 		echo $content;
+		echo $script;
 	}
 
 	public function getDirectoryTable($rank) {
@@ -25,7 +30,7 @@ class DirectoryPage extends Page {
 		$tableStr = "";
 		$teamArr = $core->fetchAllUsers();
 		foreach ($teamArr as $member) {
-			$tableStr .= "<tr><td>" . ($member['rank'] == 5 ? "<a href='?p=confirm&id=" . $member['id'] . "' title='Confirm user'>" : "") . $member['name'] . ($member['rank'] == 5 ? "</a>" : "") . " <span class='label label-" . ($member['rank'] == 7 ? "success"  : ($member['rank'] == 8 ? "danger" : ($member['rank'] == 9 ? "warning" : ($member['rank'] >= 10 ? "primary" : "default" ) ) ) ) . "'>" . Utils::getRankName($member['rank']) . "</span></td><td>" . $member['email'] . "</td><td>" . Utils::formatPhoneNum($member['phone']) . "</td>" . ($rank >= 7 ? "<td>" . $member['studentId'] . "</td>" . "<td>" . $member['hours'] . "</td>" . ($rank >= 10 ? "<td><a href='?p=del&id=" . $member['id'] . "'>Delete</a></td>" : "") : "") . "</tr>";
+			$tableStr .= "<tr><td>" . ($member['rank'] == 5 ? "<a href='?p=confirm&id=" . $member['id'] . "' title='Confirm user'>" : "") . $member['name'] . ($member['rank'] == 5 ? "</a>" : "") . " <span class='label label-" . ($member['rank'] == 7 ? "success"  : ($member['rank'] == 8 ? "danger" : ($member['rank'] == 9 ? "warning" : ($member['rank'] >= 10 ? "primary" : "default" ) ) ) ) . "'>" . Utils::getRankName($member['rank']) . "</span></td><td><span class='editable'>" . $member['email'] . "</span></td><td><span class='editable'>" . Utils::formatPhoneNum($member['phone']) . "</span></td>" . ($rank >= 7 ? "<td><span class='editable'>" . $member['studentId'] . "<span></td>" . "<td>" . $member['hours'] . "</td>" . ($rank >= 10 ? "<td><a href='?p=del&id=" . $member['id'] . "'>Delete</a></td>" : "") : "") . "</tr>";
 		}
 		return $tableStr;
 	}
