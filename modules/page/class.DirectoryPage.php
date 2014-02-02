585cee93a21cee6bf02f4441bf6eee929fd94a3f
dmattt98 <dmattt98@gmail.com>
2014-02-02 16:21:09 -0600

    Fix the editables script
    - Add the editable tag to all other fields needed
    - Fix the clearing when clicked again bug
    - Resolve minor JS error related to using jQuery on a DOM object

--- modules/page/class.DirectoryPage.php
+++ modules/page/class.DirectoryPage.php
@@ -20,10 +20,10 @@ class DirectoryPage extends Page {
         $script =
 '
 <script>
-$("span.editable").on("click",function(){$(this).html(\'<input type="text" class="form-control edit input-sm" value="\'+$(this).text()+\'" autocomplete="off">\');$("input.form-control.edit").focus();$("input.edit").focusout(function(){$(this).replaceWith($(this).val())})})
+$(".editable").click(function(){$(this).html(\'<input type="text" class="form-control edit input-sm" value="\'+(($(this).children().length==0)?($(this).text()):($(this).children()[0].value))+\'" autocomplete="off">\');$("input.form-control.edit").focus();$("input.edit").focusout(function(){$(this).replaceWith($(this).val())})})
 </script>
 ';
-        $test_usr = getUser('dmattt98@gmail.com');
+        $test_usr = $core->getUser('dmattt98@gmail.com');
         echo $content, $script, $test_usr;
     }
 
@@ -32,7 +32,7 @@ $("span.editable").on("click",function(){$(this).html(\'<input type="text" class
         $tableStr = "";
         $teamArr = $core->fetchAllUsers();
         foreach ($teamArr as $member) {
-            $tableStr .= "<tr><td>" . ($member['rank'] == 5 ? "<a href='?p=confirm&id=" . $member['id'] . "' title='Confirm user'>" : "") . $member['name'] . ($member['rank'] == 5 ? "</a>" : "") . " <span class='label label-" . ($member['rank'] == 7 ? "success"  : ($member['rank'] == 8 ? "danger" : ($member['rank'] == 9 ? "warning" : ($member['rank'] >= 10 ? "primary" : "default" ) ) ) ) . "'>" . Utils::getRankName($member['rank']) . "</span></td><td><span class='editable'>" . $member['email'] . "</span></td><td><span class='editable'>" . Utils::formatPhoneNum($member['phone']) . "</span></td>" . ($rank >= 7 ? "<td><span class='editable'>" . $member['studentId'] . "<span></td>" . "<td>" . $member['hours'] . "</td>" . ($rank >= 10 ? "<td><a href='?p=del&id=" . $member['id'] . "'>Delete</a></td>" : "") : "") . "</tr>";
+            $tableStr .= "<tr><td>" . ($member['rank'] == 5 ? "<a href='?p=confirm&id=" . $member['id'] . "' title='Confirm user'>" : "") . $member['name'] . ($member['rank'] == 5 ? "</a>" : "") . " <span class='label label-" . ($member['rank'] == 7 ? "success"  : ($member['rank'] == 8 ? "danger" : ($member['rank'] == 9 ? "warning" : ($member['rank'] >= 10 ? "primary" : "default" ) ) ) ) . "'>" . Utils::getRankName($member['rank']) . "</span></td><td class='editable'>" . $member['email'] . "</td><td class='editable'>" . Utils::formatPhoneNum($member['phone']) . "</td>" . ($rank >= 7 ? "<td class='editable'>" . $member['studentId'] . "</td>" . "<td class='editable'>" . $member['hours'] . "</td>" . ($rank >= 10 ? "<td><a href='?p=del&id=" . $member['id'] . "'>Delete</a></td>" : "") : "") . "</tr>";
         }
         return $tableStr;
     }
