25afb18af4bb226a16d0b4850bdd9b93746f7391
Eric Ozzy Osburn <bjonesen@gmail.com>
2014-02-03 18:23:23 -0600

    Remove unneeded getUser call

--- modules/page/class.DirectoryPage.php
+++ modules/page/class.DirectoryPage.php
@@ -23,8 +23,7 @@ class DirectoryPage extends Page {
 $(".editable").click(function(){$(this).html(\'<input type="text" class="form-control edit input-sm" value="\'+(($(this).children().length==0)?($(this).text()):($(this).children()[0].value))+\'" autocomplete="off">\');$("input.form-control.edit").focus();$("input.edit").focusout(function(){$(this).replaceWith($(this).val())})})
 </script>
 ';
-        $test_usr = $core->getUser('dmattt98@gmail.com');
-        echo $content, $script, $test_usr;
+        echo $content, $script;
     }
 
     public function getDirectoryTable($rank) {
