2c19181f3a5f876e75b5d42cd93b22a070b14de4
dmattt98 <dmattt98@gmail.com>
2014-02-13 21:14:49 -0600

    add check for focusout or keypress of enter

--- assets/js/directory.js
+++ assets/js/directory.js
@@ -36,21 +36,23 @@ $(".editable").click( function(){
         $("input.edit").on('focusout keypress',
             function(e){
                 console.log(e);
-                if (oldVal !== $(this).val()) {
-                    thisObj = $(this).parent();
-                    thisObj.popover({
-                        placement: 'bottom',
-                        html: 'true',
-                        trigger: 'manual',
-                        title: 'Confirm edit',
-                        content: '<div class="btn-group"><button type="button" class="btn btn-default ladda-button" data-style="slide-up" onclick="btnConfirm(this)"><span class="ladda-label">Yes</span></button><button type="button" class="btn btn-default ladda-button" data-style="slide-up" onclick="btnDeny()"><span class="ladda-label">No</span></button></div>'
-                    }).on('hide', function(){
-                        btnDeny();
-                    });
-                    thisObj.popover('show');
-                    newVal = $(this).val();
+                if (e.type === "focusout" || e.which === 13) {
+                    if (oldVal !== $(this).val()) {
+                        thisObj = $(this).parent();
+                        thisObj.popover({
+                            placement: 'bottom',
+                            html: 'true',
+                            trigger: 'manual',
+                            title: 'Confirm edit',
+                            content: '<div class="btn-group"><button type="button" class="btn btn-default ladda-button" data-style="slide-up" onclick="btnConfirm(this)"><span class="ladda-label">Yes</span></button><button type="button" class="btn btn-default ladda-button" data-style="slide-up" onclick="btnDeny()"><span class="ladda-label">No</span></button></div>'
+                        }).on('hide', function(){
+                            btnDeny();
+                        });
+                        thisObj.popover('show');
+                        newVal = $(this).val();
+                    }
+                    $(this).replaceWith( $(this).val() );
                 }
-                $(this).replaceWith( $(this).val() );
             }
         )
     }
