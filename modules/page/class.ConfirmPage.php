9480fe6aef2e93f9f0c8b6e0e62c0a075b0633cc
Dominic Tassio <dmatt98@gmail.com>
2015-01-04 13:29:30 -0600

    Fix Confirm page

--- modules/page/class.ConfirmPage.php
+++ modules/page/class.ConfirmPage.php
@@ -7,18 +7,19 @@ class ConfirmPage extends Page {
 
 	public function writePageContent() {
 		global $core;
-		$content = 
-'
+?>
+
 <div class="jumbotron" style="font-size: medium;">
-<h1>Confirm Account</h1>
-Are you sure you want to confirm ' . $core->getUserFromId($_GET['id'])['name'] . '\'s account?
-<div class="btn-group btn-group-justified">
-<a class="btn btn-lg btn-success btn-block" href="?p=confirm&id=' . $_GET['id'] . '&conf">Yes</a>
-<a class="btn btn-lg btn-danger btn-block" href="?p=directory">No</a>
+	<h1>Confirm Account</h1>
+	Are you sure you want to confirm <?php $core->getUserFromId($_GET['id'])['name']; ?>'s account?
+	<div class="btn-group btn-group-justified">
+		<a class="btn btn-lg btn-success btn-block" href="?p=confirm&id=<?php $_GET['id']?>&conf">Yes</a>
+		<a class="btn btn-lg btn-danger btn-block" href="?p=directory">No</a>
+	</div>
 </div>
-</div>
-';
-		echo $content;
+
+<?php
+
 	}
 
 	public function confirmAccount($id) {
