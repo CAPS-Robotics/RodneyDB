5b310f3725a2ec16a7bfe74a483260f9a36b2c40
Dominic Tassio <dmatt98@gmail.com>
2015-01-07 16:57:03 -0600

    revert to old button layout

--- modules/page/class.BroadcastPage.php
+++ modules/page/class.BroadcastPage.php
@@ -41,9 +41,9 @@ $(function() {
 		<span style="margin-top: -30px; z-index: 1; position: relative; float: left; opacity: 0.7;" id="charCount">
 			<span class="label label-success">160</span>
 		</span>
-		<div class="btn-group">
-			<button type="submit" class="btn btn-lg btn-primary col-md-9 col-xs-12">Send</button>
-			<button id="sendTestButton" class="btn btn-lg btn-default col-md-3 col-xs-12">Test</button>
+		<div class="btn-group btn-group-justified">
+			<a href="#" class="btn btn-lg btn-primary" onclick="document.forms['sendForm'].submit();">Send To All</a>
+			<a href="#" id="sendTestButton" class="btn btn-lg btn-default">Send Test to Self</a>
 		</div>
 	</form>
 </div>
