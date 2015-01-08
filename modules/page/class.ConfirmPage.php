3e45f37377e8ba842bd2ccfde8895a2c32856347
Dominic Tassio <dominic.tassio@gmail.com>
2015-01-08 13:30:12 -0600

    Fix confirm button bug

--- modules/page/class.ConfirmPage.php
+++ modules/page/class.ConfirmPage.php
@@ -13,7 +13,7 @@ class ConfirmPage extends Page {
 	<h1>Confirm Account</h1>
 	Are you sure you want to confirm <?php echo $core->getUserFromId($_GET['id'])['name']; ?>'s account?
 	<div class="btn-group btn-group-justified">
-		<a class="btn btn-lg btn-success btn-block" href="?p=confirm&id=<?php $_GET['id']?>&conf">Yes</a>
+		<a class="btn btn-lg btn-success btn-block" href="?p=confirm&id=<?php echo $_GET['id']; ?>&conf">Yes</a>
 		<a class="btn btn-lg btn-danger btn-block" href="?p=directory">No</a>
 	</div>
 </div>
