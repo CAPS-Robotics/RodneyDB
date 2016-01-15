4b0ae3ea95bd5e678e56966f790af09c69078a63
Dominic Tassio <dominic.tassio@gmail.com>
2016-01-14 19:27:06 -0600

    Update checkin.js

--- assets/js/checkin.js
+++ assets/js/checkin.js
@@ -51,7 +51,7 @@ $(document).ready(function(){
             $("#checkin").click();
         }
     });
-    var timer = countdown(new Date(2016, 1, 17, 20, 0, 0, 0), function(ts) {
+    var timer = countdown(new Date(2016, 1, 23, 20, 0, 0, 0), function(ts) {
         document.getElementById('clock').innerHTML = ts.toString();
     }, countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
 });
